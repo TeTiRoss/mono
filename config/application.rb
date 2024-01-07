935e6fa918e76ebecd0786cb8cd394b86f6ea07f
rostyslav.semanyshyn <rostyslav.semanyshyn@n-able.com>
2024-01-07 19:15:32 +0200

    update bundler

--- config/application.rb
+++ config/application.rb
@@ -20,9 +20,6 @@ Bundler.require(*Rails.groups)
 
 module Mono
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
-
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
