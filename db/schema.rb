ef04fe85c8d47cb423c0494163c0bfab7cb67038
rostyslav.semanyshyn <tetiross@gmail.com>
2024-11-23 21:18:35 +0200

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -2,18 +2,17 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_25_131240) do
-
+ActiveRecord::Schema[8.0].define(version: 2024_01_07_170806) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "currency_rates", force: :cascade do |t|
     t.jsonb "raw_data"
@@ -23,16 +22,15 @@ ActiveRecord::Schema.define(version: 2020_04_25_131240) do
     t.decimal "rate_buy", precision: 13, scale: 4
     t.decimal "rate_sell", precision: 13, scale: 4
     t.decimal "rate_cross", precision: 13, scale: 4
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "transactions", force: :cascade do |t|
     t.jsonb "raw_data"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "mono_id"
     t.index ["mono_id"], name: "index_transactions_on_mono_id", unique: true
   end
-
 end
