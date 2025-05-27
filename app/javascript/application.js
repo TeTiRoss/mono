b457326298f226fe067ebae410a9e78626013abe
rostyslav.semanyshyn <tetiross@gmail.com>
2025-05-27 16:38:33 +0300

    add flash[:notice] display on the page

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+document.addEventListener('DOMContentLoaded', () => {
+  const flashMessages = document.querySelectorAll('.alert');
+  flashMessages.forEach((el) => {
+    setTimeout(() => {
+      el.style.transition = 'opacity 0.5s ease-out';
+      el.style.opacity = '0';
+      setTimeout(() => el.remove(), 500); // remove from DOM after fade out
+    }, 3000); // 2 seconds
+  });
+});
