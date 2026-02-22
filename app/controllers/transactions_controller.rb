bd870a0148a8305f4afc351bf9dff244b272a679
rostyslav.semanyshyn <tetiross@gmail.com>
2026-02-22 21:12:34 +0200

    add sync by time period

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -10,8 +10,26 @@ class TransactionsController < ApplicationController
   end
 
   def sync
-    TransactionsFetchWorker.new.perform 1.week.ago, Time.current
+    from_time, to_time = parse_sync_period(params[:from_date], params[:to_date])
 
-    redirect_to transactions_path, notice: 'Transactions were synced for the week period'
+    if from_time > to_time
+      return redirect_to transactions_path, alert: 'From date must be before To date'
+    end
+
+    TransactionsFetchWorker.new.perform from_time, to_time
+
+    redirect_to transactions_path, notice: "Transactions were synced for #{from_time.to_date}..#{to_time.to_date}"
+  end
+
+  private
+
+  def parse_sync_period(from_date, to_date)
+    from_value = from_date.presence || 1.week.ago.to_date.iso8601
+    to_value   = to_date.presence || Date.current.iso8601
+
+    from_time = Date.iso8601(from_value).in_time_zone.beginning_of_day
+    to_time   = Date.iso8601(to_value).in_time_zone.end_of_day
+
+    [from_time, to_time]
   end
 end
